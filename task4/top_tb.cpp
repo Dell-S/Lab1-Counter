050cd05ff5544b665ac7e5fe88aaab2c116676d1
mello-marsh <48381979+mello-marsh@users.noreply.github.com>
2023-11-07 16:36:11 +0000

    Lab 1 Complete

--- task4/top_tb.cpp
+++ task4/top_tb.cpp
@@ -16,14 +16,14 @@ int main(int argc, char **argv, char **env) {
     tfp->open("top.vcd");
     
     if (vbdOpen()!=1) return(-1);
-    vbdHeader("BCD");
+    vbdHeader("Lab1: BCD");
 
     top_module->clk = 1;     
     top_module->rst = 1;     
     top_module->en = 0;
-    top_module->v = 0x11;       
+    top_module->v = vbdValue();       
 
-    for (i = 0; i < 300; i++) {
+    for (i = 0; i < 1000; i++) {
         for (clk = 0; clk < 2; clk++) {
             tfp->dump(2 * i + clk);
             top_module->clk = !top_module->clk;
@@ -31,17 +31,16 @@ int main(int argc, char **argv, char **env) {
         }
 
         //++++ send BCD value to VBuddy
-
-        vbdHex(4, (int(top_module->bcd) >> 16) & 0xF);
-        vbdHex(3, (int(top_module->bcd) >> 8) & 0xF);
-        vbdHex(2, (int(top_module->bcd) >> 4) & 0xF);
-        vbdHex(1, int(top_module->bcd) & 0xF);
+        vbdHex(4, (int(top_module->bcd) >> 8) & 0xF);
+        vbdHex(3, (int(top_module->bcd) >> 4) & 0xF);
+        vbdHex(2, int(top_module->bcd) & 0xF);
 
         //vbdPlot(int(top_module->bcd), 0, 999);  
         vbdCycle(i+1);
 
-        top_module->rst = (i<2) | (i==15);
+        top_module->rst = (i<2);
         top_module->en = vbdFlag();
+        top_module->v = vbdValue()
         if (Verilated::gotFinish())  exit(0);
     }
 
